6e61fb5bb4012dc92cc20e77ba296e980eb004f6
anvg <alanavuong@gmail.com>
2020-07-04 23:44:23 -0500

    Update the gradle

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -22,6 +22,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+/* homepage that contains the categories for the language */
 public class MainActivity extends AppCompatActivity {
 
     @Override
